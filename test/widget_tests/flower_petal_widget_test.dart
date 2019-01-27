5777e671cfc108666872234cd66e78b88e641816
Owlaukka <olli.laukkanen@protonmail.com>
2019-01-28 01:32:12 +0200

    widget test for Flower

---
+++ test/widget_tests/flower_petal_widget_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:learning_compass_exp/screens/home/widgets/flower_petal.dart';
+
+void main() {
+  /*
+  final TestWidgetsFlutterBinding binding =
+      TestWidgetsFlutterBinding.ensureInitialized();
+  if (binding is LiveTestWidgetsFlutterBinding) {
+    binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+  }
+
+  group("FlowerPetal widget", () {
+    testWidgets("works", (WidgetTester tester) async {
+
+      await tester.pumpWidget(
+        FlowerPetal(),
+      );
+
+      await tester.pump();
+      await tester.pump();
+
+      Container container = find.byType(Container).evaluate().first.widget;
+      expect(container.child, equals(Transform));
+    });
+  });
+*/
+}
