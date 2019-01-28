9da7aaf0583377d9f02849c49bfb14bf2c58043d
Anafi Babatunde O <banafi@sf-2.cs.helsinki.fi>
2019-01-28 17:55:15 +0200

    Drawer skelenton

---
+++ lib/screens/home/widgets/drawer/drawer_header.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class MDrawerHeader {
+  String name;
+  String email;
+  IconData photo;
+  MDrawerHeader (this.name,this.email, this.photo);
+}
+
+class MDrawerHeaderWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+
+
+}
+
+}
---
+++ lib/screens/home/widgets/drawer/list_tile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MListTile{
+  String title;
+  IconData icon;
+  MListTile(this.title, this.icon);
+
+}
+
+
+class MListTileWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
