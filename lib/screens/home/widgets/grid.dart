9f0671c79019196315b3626e49c6450592032e44
Krista Lindholm <krista.lindholm@gmail.com>
2019-01-29 21:50:51 +0200

    Add grid to home screen

---
+++ lib/screens/home/widgets/grid.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:learning_compass_exp/screens/home/widgets/grid_item.dart';
+
+class Grid extends StatelessWidget {
+  // The item data should probably come from somewhere else in the future
+  final List _items = [
+    { 'name': 'Jobs', 'color': Colors.blue },
+    { 'name': 'Income', 'color': Colors.cyan },
+    { 'name': 'Housing', 'color': Colors.teal[300] },
+    { 'name': 'Work Life Balance', 'color': Colors.red[600] },
+    { 'name': 'Safety', 'color': Colors.blueGrey },
+    { 'name': 'Life Satisfaction', 'color': Colors.orange[600] },
+    { 'name': 'Health', 'color': Colors.purple },
+    { 'name': 'Civic Engagement', 'color': Colors.amber },
+    { 'name': 'Environment', 'color': Colors.green },
+    { 'name': 'Education', 'color': Colors.lightGreen[400] },
+    { 'name': 'Community', 'color': Colors.red[400] },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    bool _isVertical = MediaQuery.of(context).orientation == Orientation.portrait;
+    return Column(
+      children: [
+        SizedBox (
+          width: _isVertical ? 350.0 : 525.0,
+          child: GridView.count(
+            shrinkWrap: true,
+            primary: false,
+            crossAxisCount: _isVertical ? 2 : 3,
+            padding: const EdgeInsets.all(10.0),
+            children: _items.map((item) => GridItem(item: item)).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+}
