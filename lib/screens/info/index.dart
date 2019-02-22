7902d22bff11751dd8d9cf98b5c8ebfe4347fb4b
Krista Lindholm <krista.lindholm@gmail.com>
2019-02-22 23:50:26 +0200

    Modify state to forget subroute when leaving info screen

--- lib/screens/info/index.dart
+++ lib/screens/info/index.dart
@@ -5,6 +5,7 @@ import 'package:redux/redux.dart';
 import 'package:learning_compass_exp/data/models/petal_names.dart';
 import 'package:learning_compass_exp/screens/info/info_view.dart';
 import 'package:learning_compass_exp/store/app_state.dart';
+import 'package:learning_compass_exp/store/actions/actions.dart';
 
 class InfoScreen extends StatelessWidget {
   final PetalName name;
@@ -22,29 +23,36 @@ class InfoScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // the color will be read from the petal constants in the future
     Color _color = Colors.orange[600];
+    Future<bool> _willPopCallback(Function onPop) async {
+      await onPop();
+      return true;
+    }
 
-    return StoreConnector<AppState, String>(
-    converter: (Store<AppState> store) => store.state.subroute,
-    builder: (context, subroute) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('$name'),
-            backgroundColor: _color,
-          ), body: Center(child: Column(children: [
-            Text('add the chapter index/menu here'),
-            RaisedButton(
-              child: Text('chapter button'),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) =>
-                        InfoView(name: name, color: _color, chapters: chapters,)
-                    )
-                );
-              },
-            )
-      ])),
-      );
-    });
+    return StoreConnector<AppState, Function>(
+      converter: (Store<AppState> store) {
+        return () => store.dispatch(ChangeSubrouteAction(null));
+      },
+      builder: (context, onPopCallback) {
+        return WillPopScope(child: Scaffold(
+            appBar: AppBar(
+              title: Text('$name'),
+              backgroundColor: _color,
+            ), body: Center(child: Column(children: [
+              Text('add the chapter index/menu here'),
+              RaisedButton(
+                child: Text('chapter button'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>
+                          InfoView(name: name, color: _color, chapters: chapters,)
+                      ),
+                  );
+                },
+              ),
+        ]),
+        ),
+        ), onWillPop: () => _willPopCallback(onPopCallback),);
+      });
     }
 }
