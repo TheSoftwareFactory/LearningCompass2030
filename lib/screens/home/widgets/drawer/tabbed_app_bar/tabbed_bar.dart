528ed6c5d9800d25e501eb3a0656655981355927
Babatund Anafi <labnof@gmail.com>
2019-02-26 00:48:29 +0200

    tabbed_choice_body added

--- lib/screens/home/widgets/drawer/tabbed_app_bar/tabbed_bar.dart
+++ lib/screens/home/widgets/drawer/tabbed_app_bar/tabbed_bar.dart
@@ -4,7 +4,7 @@ import 'package:learning_compass_exp/screens/home/widgets/drawer/tabbed_app_bar/
 
 
 //this will come from json
-const List choicesBodies = [
+const List<ChoiceBody> choicesBodies = const <ChoiceBody>[
   const ChoiceBody('title', 'body'),
   const ChoiceBody('title', 'body'),
   const ChoiceBody('title', 'body'),
@@ -14,9 +14,9 @@ const List choicesBodies = [
 //this will come from json
 const List<Choice> choices = const <Choice>[
   const Choice(title: 'OECD', body: choicesBodies ),
-  const Choice(title: 'TRENDS' ),
-  const Choice(title: 'FRAMEWORK',  ),
-  const Choice(title: 'ANALYSIS',  ),
+  const Choice(title: 'TRENDS', body: choicesBodies ),
+  const Choice(title: 'FRAMEWORK',  body: choicesBodies ),
+  const Choice(title: 'ANALYSIS',  body: choicesBodies ),
 ];
 
 
