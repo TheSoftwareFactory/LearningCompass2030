3316c18e00215b983a499d298c7df846a9644782
Anafi Babatunde O <banafi@sf-2.cs.helsinki.fi>
2019-03-01 20:05:16 +0200

    added json decoded

--- lib/screens/home/widgets/drawer/tabbed_app_bar/tabbed_bar.dart
+++ lib/screens/home/widgets/drawer/tabbed_app_bar/tabbed_bar.dart
@@ -4,38 +4,71 @@ import 'package:learning_compass_exp/screens/home/widgets/drawer/tabbed_app_bar/
 
 class AboutTabbedAppBar extends StatelessWidget {
 
-  static final _choicesJson = 'assets/static_data/choices.json';
-  final List< Map <String, dynamic>> _choices = json.decode(_choicesJson)['choice'];
 
+  List<Widget> _convertJsonToWidget(List<dynamic> json) {
+    List<Widget> newList = new List<Widget>();
+
+    json.forEach((element) {
+      newList.add(Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ChoiceWidget(choice: element),
+      ));
+    });
+
+    return newList;
+  }
+
+  List<Widget> _convertJsonToTabTab(List<dynamic> json) {
+    List<Widget> newList = new List<Widget>();
+
+    json.forEach((element) {
+      newList.add(Tab(text: element['title'],
+      ));
+    });
+
+    return newList;
+  }
+
+  static final _choicesJson = 'assets/static_data/choices.json';
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: DefaultTabController(
-        length: _choices.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('About Learning Compass'),
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: _choices.map((Map <String, dynamic> choice ) {
-                return Tab(
-                  text: choice ['title'],
+    return FutureBuilder<String>(
+      future: DefaultAssetBundle.of(context).loadString(_choicesJson),
+      builder: (context, snapshot){
+        if (snapshot.hasData) {
+         var  _choices = json.decode(snapshot.data)['choices'];
+         print(_choices);
 
-                );
-              }).toList(),
+          return  DefaultTabController(
+            length: _choices.length,
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text('About Learning Compass'),
+                bottom: TabBar(
+                  isScrollable: true,
+                  tabs: _convertJsonToTabTab(_choices),
+                ),
+              ),
+              body: TabBarView(
+                children: _convertJsonToWidget(_choices),
+              ),
             ),
-          ),
-          body: TabBarView(
-            children: _choices.map((Map <String, dynamic> choice) {
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ChoiceWidget(choice: choice),
-              );
-            }).toList(),
-          ),
-        ),
-      ),
+          );
+
+
+
+
+
+
+        }
+        return Center(
+          child: Text("adsfadsfadsf"),
+        );
+      }
+
     );
+
   }
+
 }
